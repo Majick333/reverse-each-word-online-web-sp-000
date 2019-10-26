860179ce7529fe74f9f88932d8c24815d22ecfb7
Ryan M <rmccauleymusic@gmail.com>
2019-10-26 15:40:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(sentence)
   nuarray = sentence.split(" ")
     test_array = []
   nuarray.collect  do|word|
-    test_array << #{word.reverse}
+    test_array << word.reverse
     end
 test_array.join
 end
