f64348be9f664e369b6711400ff4d9a8f70a161e
Ryan M <rmccauleymusic@gmail.com>
2019-10-26 15:17:12 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+sentence = ()
+
+
+def reverse_each_word(sentence)
+
+  answer = []
+  finanswer = []
+  #sentence.reverse
+  answer.push(sentence.split(" "))
+  answer.each { |word| finanswer.push("#{word.reverse}")}
+  finanswer.collect do |elements| elements.reverse
+  
+
+end
+#return elements()
+end
